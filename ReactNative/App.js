1bb14c816ea1363eb4d75734d6cf614eea9ad140
tweenietomatoes <emre@temelkuran.ist>
2019-10-17 22:13:45 +0300

    Update App.js

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -7,21 +7,16 @@
  */
 
 import React from 'react';
-import {
-  View,
-  ActivityIndicator
-} from 'react-native';
-import { Container, Header, Left, Body, Right, Title, Text, Button } from 'native-base';
+import { View, ActivityIndicator } from 'react-native';
+import { Container, Header, Left, Body, Right, Title, Text, Button, H1, Content, Badge, Footer, Icon } from 'native-base';
 import { Row, Grid } from 'react-native-easy-grid';
 
 export default class App extends React.Component {
    constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state ={ isLoading: true, dataSource: null}
   }
-
-  //abortController = new AbortController();
-
+ 
   componentDidMount(){
     return fetch('https://api.temelkuran.ist/get')
       .then((response) => response.json())
@@ -34,30 +29,20 @@ export default class App extends React.Component {
 
         });
 
-      }).catch((error) => {
+      })
+      .catch((error) =>{
         console.error(error);
       });
-    
   }
-/*
-, {signal: this.abortController.signal}
 
-  .catch((error) =>{
-        if (error.name === 'AbortError') return
-        throw error
-      });
-
-  componentWillUnmount() {
-    this.abortController.abort();
-  }
-*/
    render(){
-
+    
     if(this.state.isLoading){
       return(
-        <View style={{flex: 1, padding: 20}}>
-          <Text>Fetching...</Text><ActivityIndicator/>
-        </View>
+        <Container>
+        <H1>App is loading...</H1>
+        <ActivityIndicator/>
+        </Container>
       );
     }
 
@@ -70,11 +55,15 @@ export default class App extends React.Component {
      </Body>
      <Right><Text>Y</Text></Right>
    </Header>
-<Grid>
- 
 
- <Row size={5} />
-  <Row size={75}>
+   <Content>
+<Grid>
+
+<Row>
+<Button success><Text>Time: {this.state.dataSource.time}</Text></Button>
+</Row>
+
+<Row>
 <Button danger>
 <Text>{this.state.dataSource.temperature} C</Text>
 </Button>
@@ -87,12 +76,21 @@ export default class App extends React.Component {
 <Text>{this.state.dataSource.humidity} %rh</Text>
  </Button>
 
+</Row>
+
+<Row>
 <Button warning>
 <Text>{this.state.dataSource.gas} Kohm</Text>
  </Button>
 
+ <Button dark>
+<Text>{this.state.dataSource.altitude} Kohm</Text>
+ </Button>
 </Row>
+
 </Grid>
+</Content>
+<Footer><Text><Icon name='home' style={{color: 'white'}}/></Text></Footer>
 </Container>
     );
   }
