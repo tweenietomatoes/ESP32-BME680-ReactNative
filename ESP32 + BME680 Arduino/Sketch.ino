0e62791b9ad11e4fd45d77dbd7e76097c3908cad
tweenietomatoes <emre@temelkuran.ist>
2019-10-18 12:10:53 +0300

    Create Sketch.ino

---
+++ ESP32 + BME680 Arduino/Sketch.ino
@@ -0,0 +1,163 @@
+  
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+#include <WiFi.h>
+#include<WiFiClientSecure.h>
+#include <WiFiMulti.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (997)
+
+Adafruit_BME680 bme; // I2C
+
+const String uri = "https://api.temelkuran.ist";
+const char *host = "api.temelkuran.ist";
+const uint16_t port = 443;
+//const int32_t timeout = 500;
+
+WiFiMulti WiFiMulti;
+
+void setup()
+{
+    Serial.begin(115200);
+  while (!Serial);
+  Serial.println(F("BME680 async test"));
+
+  if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    while (1);
+  }
+  
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150);
+
+ WiFiMulti.addAP("TP-Link_DDC9", "patlican123");
+ Serial.println();
+ Serial.println();
+ Serial.print("WiFi Connecting...");
+ 
+ while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+  Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+       delay(1000);
+}
+
+void loop()
+{
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
+    return;
+  }
+  Serial.print(F("Reading started at "));
+  Serial.print(millis());
+  Serial.print(F(" and will finish at "));
+  Serial.println(endTime);
+
+  Serial.println(F("You can do other work during BME680 measurement."));
+  delay(50);
+
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
+  Serial.print(F("Reading completed at "));
+  Serial.println(millis());
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(F(" hPa"));
+
+  Serial.print(F("Humidity = "));
+  Serial.print(bme.humidity);
+  Serial.println(F(" %"));
+
+  Serial.print(F("Gas = "));
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(F(" KOhms"));
+
+  Serial.print(F("Approx. Altitude = "));
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(F(" m"));
+
+  Serial.println();
+
+   Serial.print("Connecting to ");
+   Serial.println(host);
+
+   WiFiClientSecure client;
+
+   const char* root_ca = \
+  "-----BEGIN CERTIFICATE-----\n" \
+"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n" \
+"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n" \
+"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" \
+"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n" \
+"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n" \
+"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n" \
+"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n" \
+"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n" \
+"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n" \
+"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n" \
+"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n" \
+"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n" \
+"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n" \
+"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n" \
+"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n" \
+"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
+"-----END CERTIFICATE-----\n";
+
+   client.setCACert(root_ca); 
+   
+    if (!client.connect(host, port)) {
+        Serial.println("Connection failed.");
+        Serial.println("Waiting 5 seconds before retrying...");
+        delay(5000);
+        return;
+    }
+    
+  String body = 
+  "temperature=" + String(bme.temperature) +
+  "&humidity=" + String(bme.humidity) +
+  "&pressure=" + String(bme.pressure/100) +
+  "&gas=" + String(bme.gas_resistance/1000) +
+  "&altitude=" + String(bme.readAltitude(SEALEVELPRESSURE_HPA)); 
+
+  String postRequest = 
+  "POST " + uri + " HTTP/1.1\r\n" + 
+  "Host: " + host + "\r\n" + 
+  "Content-Type: application/x-www-form-urlencoded\r\n" +
+  "Content-Length: " + body.length() + "\r\n" +
+  "\r\n" + body;
+  
+  Serial.println(postRequest);
+  client.print(postRequest);
+  client.stop();
+
+  Serial.println();
+  Serial.println("***DONE***");
+  Serial.println();
+  delay(6000);
+}
